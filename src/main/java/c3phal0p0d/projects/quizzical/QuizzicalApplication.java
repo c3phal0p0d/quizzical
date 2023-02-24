dc8f6a9560b9e0ba2795239f0a315e8c6b3f8875
nchiorsac <natasha@okapps.net>
2023-02-24 16:06:34 +1100

    initial commit

---
+++ src/main/java/c3phal0p0d/projects/quizzical/QuizzicalApplication.java
@@ -0,0 +1,13 @@
+package c3phal0p0d.projects.quizzical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/c3phal0p0d/projects/quizzical/QuizzicalApplicationTests.java
@@ -0,0 +1,13 @@
+package c3phal0p0d.projects.quizzical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
